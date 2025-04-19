9022418a87d197fba4d05f538151616725898972
Poovarasan Parthasarathy <arasan_21@hotmail.com>
2025-04-19 21:27:32 +0530

    DiscountOfProduct

---
+++ l9Loops/src/com/dsa/assignments/DiscountOfProduct.java
@@ -0,0 +1,23 @@
+package com.dsa.assignments;
+
+import java.util.Scanner;
+
+public class DiscountOfProduct {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter product price :");
+		double productPrice = in.nextDouble();
+		System.out.println("Enter discount percentage :");
+		double discount = in.nextDouble();
+		if(discount > 100) {
+			System.out.println("Discount cannot be greater than 100");
+			System.exit(0);
+		}
+		double discountAmount = (productPrice*(discount/100));
+		double finalPrice = productPrice - discountAmount;
+		System.out.println("Saved amount : "+discountAmount);
+		System.out.println("Price after discount: "+finalPrice);
+	}
+
+}
