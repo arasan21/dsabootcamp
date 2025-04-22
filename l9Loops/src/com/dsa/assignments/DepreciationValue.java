a62e1a5fac584792dd6cfee550e2c9ad706507a7
Poovarasan Parthasarathy <arasan_21@hotmail.com>
2025-04-22 21:48:20 +0530

    DepreciationValue

---
+++ l9Loops/src/com/dsa/assignments/DepreciationValue.java
@@ -0,0 +1,19 @@
+package com.dsa.assignments;
+
+import java.util.Scanner;
+
+public class DepreciationValue {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter the amount:");
+		double amount = in.nextDouble();
+		System.out.println("Enter the depreciation rate:");
+		double  depreciationRate= in.nextDouble();
+		System.out.println("Enter the time duration in years:");
+		double  timeInYrs= in.nextDouble();
+		double depreciationAmount = amount * Math.pow(1-(depreciationRate/100),timeInYrs);
+		System.out.println("Depreciated Value : "+depreciationAmount);
+	}
+
+}
