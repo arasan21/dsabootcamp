e170e67557830230bbbcb8c4779fb293071d0ef9
Poovarasan Parthasarathy <arasan_21@hotmail.com>
2025-04-23 22:19:56 +0530

    ArmstrongNumber

---
+++ l9Loops/src/com/dsa/assignments/ArmstrongNumber.java
@@ -0,0 +1,25 @@
+package com.dsa.assignments;
+
+import java.util.Scanner;
+
+public class ArmstrongNumber {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter the number");
+		int number= in.nextInt();
+		int sum =0;
+		int temp = number;
+		while(temp > 0) {
+			int rem =  temp%10;
+			sum += rem*rem*rem;
+			temp /=10;
+		}
+		if(sum == number) {
+			System.out.println(number+" is an Armstrong number");
+		}else {
+			System.out.println(number+" is an not an Armstrong number");
+		}
+	}
+
+}
