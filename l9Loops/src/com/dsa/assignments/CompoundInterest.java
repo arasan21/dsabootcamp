617b84eccd551ada0ef6f06e9cee7d95c407c2af
Poovarasan Parthasarathy <arasan_21@hotmail.com>
2025-04-23 22:10:07 +0530

    CI

---
+++ l9Loops/src/com/dsa/assignments/CompoundInterest.java
@@ -0,0 +1,19 @@
+package com.dsa.assignments;
+
+import java.util.Scanner;
+
+public class CompoundInterest {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter principle amount:");
+		double principle = in.nextDouble();
+		System.out.println("Enter the rate:");
+		double rate = in.nextDouble();
+		System.out.println("Enter the time:");
+		double time = in.nextDouble();
+		double compoundInterest = principle * Math.pow((1+rate/100), time);
+		System.out.println("Compound Interest: "+compoundInterest);
+	}
+
+}
