0670a9ada4c700ab8b93958be54dbe9bd1fec244
Poovarasan Parthasarathy <arasan_21@hotmail.com>
2025-04-23 22:13:09 +0530

    SumOfNNumbers

---
+++ l9Loops/src/com/dsa/assignments/SumOfNNumbers.java
@@ -0,0 +1,14 @@
+package com.dsa.assignments;
+
+import java.util.Scanner;
+
+public class SumOfNNumbers {
+
+	public static void main(String[] args) {
+		System.out.println("Enter the number");
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		System.out.println("Sum of "+n+" numbers : "+n*(1+n)/2);
+	}
+
+}
