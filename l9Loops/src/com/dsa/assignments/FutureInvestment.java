c8984b74270882003770629cc54e1b18506a6f63
Poovarasan Parthasarathy <arasan_21@hotmail.com>
2025-04-24 08:06:20 +0530

    FutureInvestment

---
+++ l9Loops/src/com/dsa/assignments/FutureInvestment.java
@@ -0,0 +1,25 @@
+package com.dsa.assignments;
+
+import java.util.Scanner;
+
+public class FutureInvestment {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter investment amount:");
+		double pv = in.nextDouble();
+		System.out.println("Enter the interest rate:");
+		double r = in.nextDouble();
+		System.out.println("Enter number of peroid:");
+		int n = in.nextInt();
+		System.out.println("Future Value : ");
+		for(int i=1;i<=n;i++) {
+			 System.out.printf("%d\t%.2f%n", i, futureValue(pv, r, i));
+		}
+	}
+	
+	static double futureValue(double pv, double r,int n) {
+		return pv * Math.pow((1+r/100), n);
+	}
+
+}
