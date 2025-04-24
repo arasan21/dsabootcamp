8ee7c6e1f5dc4a7feeafb46e6b4de9655a83813d
Poovarasan Parthasarathy <arasan_21@hotmail.com>
2025-04-24 09:57:47 +0530

    VowelOrConsonant

---
+++ l9Loops/src/com/dsa/assignments/VowelOrConsonant.java
@@ -0,0 +1,20 @@
+package com.dsa.assignments;
+
+import java.util.Scanner;
+
+public class VowelOrConsonant {
+
+	public static void main(String[] args) {
+		System.out.println("Enter an alphabet");
+		Scanner in = new Scanner(System.in);
+		String str = in.next();
+		switch(str.charAt(0)) {
+		case 'a','e','i','o','u':
+				System.out.println(str.charAt(0)+" is a Vowel");
+			break;
+			default:
+				System.out.println(str.charAt(0)+" is a Consonant");
+		}
+	}
+
+}
