e79b2ce46a64476cbca21777d2871a000488ef02
Poovarasan Parthasarathy <arasan_21@hotmail.com>
2025-05-03 21:15:03 +0530

    BuildArrayPermutation

---
+++ l9Loops/src/com/dsa/array/BuildArrayPermutation.java
@@ -0,0 +1,25 @@
+package com.dsa.array;
+
+import java.util.Arrays;
+
+//	https://leetcode.com/problems/build-array-from-permutation/description/
+/**
+ * Given a zero-based permutation nums (0-indexed), build an array ans of the same length
+ *  where ans[i] = nums[nums[i]] for each 0 <= i < nums.length and return it.
+ *  A zero-based permutation nums is an array of distinct integers from 0 to nums.length - 1 (inclusive).
+ **/
+public class BuildArrayPermutation {
+
+	public static void main(String[] args) {
+		int[] nums = {0,2,1,5,3,4};
+		System.out.println(Arrays.toString(buildArray(nums)));
+	}
+
+	static int[] buildArray(int[] nums) {
+        int[] ans = new int[nums.length];
+        for(int i=0; i < nums.length;i++){
+            ans[i]=nums[nums[i]];
+        }
+        return ans;
+    }
+}
--- l9Loops/src/com/dsa/assignments/conditionsloops/SumOfNNumbers.java
+++ l9Loops/src/com/dsa/assignments/conditionsloops/SumOfNNumbers.java
@@ -9,6 +9,19 @@ public class SumOfNNumbers {
 		Scanner in = new Scanner(System.in);
 		int n = in.nextInt();
 		System.out.println("Sum of "+n+" numbers : "+n*(1+n)/2);
+		//Sum of first n natural numbers = (n * (n+1)) / 2
+		
+		// n natural numbers that avoids overflow if result is going to be within limits.
+		
+		// Returns sum of first n natural
+	    // numbers
+//	        if (n % 2 == 0)
+//	            return (n / 2) * (n + 1);
+//
+//	        // If n is odd, (n+1) must be even
+//	        else
+//	            return ((n + 1) / 2) * n;
+
 	}
 
 }
