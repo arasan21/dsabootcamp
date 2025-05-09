31489f50127669a7e60cdb26a5e971353a00116f
Poovarasan Parthasarathy <arasan_21@hotmail.com>
2025-05-09 22:45:00 +0530

    DiagonalSum

---
+++ l9Loops/src/com/dsa/array/DiagonalSum.java
@@ -0,0 +1,29 @@
+package com.dsa.array;
+
+public class DiagonalSum {
+
+	public static void main(String[] args) {
+		int[][] mat = {{1,2,3},
+		               {4,5,6},
+		               {7,8,9}};
+		System.out.println(diagonalSum(mat));
+	}
+	
+	public static int diagonalSum(int[][] mat) {
+        int sum =0;
+        for(int i=0; i < mat.length; i++){
+            for(int j=0; j < mat[i].length; j++){
+                if(i==j){
+                    sum += mat[i][j];
+                }else{
+                    if(i+j == mat.length-1){
+                        sum += mat[i][j];
+                    }
+                }
+                System.out.println(sum);
+            }
+        }
+        return sum;
+    }
+
+}
